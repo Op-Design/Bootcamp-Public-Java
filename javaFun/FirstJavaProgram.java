4ba53637157b6d1d9106bf75ab0b19e1d0c64d0c
Op-Design <o.immanuel.o@gmail.com>
2021-09-18 15:19:44 -0500

    javaFun updated

---
+++ javaFun/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram{
+    public static void main (String[] args){
+        System.out.println("My name is Opeyemi Olaleye");
+        System.out.println("I am 260 years old now");
+        System.out.println("My hometown is . . .");
+    }
+}
