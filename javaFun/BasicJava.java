80db8a303741aee7ddf81ea6fdad94dfd499ebff
Op-Design <o.immanuel.o@gmail.com>
2021-11-27 18:40:39 -0500

    BasicJava update

--- javaFun/BasicJava.java
+++ javaFun/BasicJava.java
@@ -1,10 +1,12 @@
+// import java.util.Iterator;
+
 public class BasicJava {
     
     public void printStart1Through255() {
         int [] myArray = new int [255];
-        for (int i = 0; i < 255; i++){
-            myArray[i] = i+1;
-            System.out.print(myArray[i]);
+        for (int iterator = 0; iterator < 255; iterator++){
+            myArray[iterator] = iterator+1;
+            System.out.print(myArray[iterator]);
         }
         // for (int i = 0; i < 7; i++){
         //     System.out.println("bar");
@@ -15,4 +17,33 @@ public class BasicJava {
     Print 1-255
     Write a method that prints all the numbers from 1 to 255.
     */
+
+    public void printOddStart1Through255(){
+        int [] myArray = new int [255];
+        for (int iterator = 0; iterator < 255; iterator++){
+            if (iterator%2 == 0){
+                myArray[iterator] = iterator+1;
+                System.out.print(myArray[iterator]);
+            }
+        }
+    }
+
+    public int sum255(){
+        int result = 0;
+        for (int iterator=0; iterator<255; iterator++){
+            result+=iterator+1;
+        }
+        return result;
+    }
+
+    public void iterateThroughArray(int [] array){
+        // System.out.println(array.length);
+        int iterator = 0;
+        while(iterator < array.length){
+            System.out.println(array[iterator]);
+            iterator++;
+        }
+    }
+
+
 }
--- javaFun/BasicJavaTest.java
+++ javaFun/BasicJavaTest.java
@@ -1,6 +1,15 @@
 public class BasicJavaTest {
     public static void main(String [] arg){
         BasicJava myArray = new BasicJava();
-        myArray.printStart1Through255();
+
+        // myArray.printStart1Through255();
+        
+        // myArray.printOddStart1Through255();
+        
+        // int result = myArray.sum255();
+        // // System.out.println(result);
+
+        int [] arrayToIterate = {1,3,5,7,9,13};
+        myArray.iterateThroughArray(arrayToIterate);
     }
 }
